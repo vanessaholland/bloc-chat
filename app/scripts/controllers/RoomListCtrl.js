bd2f4723d6321af9092591fd7b539ee09618eee7
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-12-02 12:11:14 -0600

    wip

--- app/scripts/controllers/RoomListCtrl.js
+++ app/scripts/controllers/RoomListCtrl.js
@@ -1,18 +1,9 @@
 (function() {
-    function RoomListCtrl($firebaseArray) {
-         var ref = firebase.database().ref().child("rooms");
-         var rooms = $firebaseArray(ref);
-
-         rooms.$loaded().then(function(_rooms){
-             console.log("rooms: " + _rooms.length);
-         });
-         return {
-           all: rooms
-         };
+    function RoomListCtrl(Room) {
+        this.roomList = Room;
        }
-
 
      angular
          .module('blocChat')
-         .controller('RoomListCtrl', ['$firebaseArray', RoomListCtrl]);
+         .controller('RoomListCtrl', ['Room', RoomListCtrl]);
  })();
