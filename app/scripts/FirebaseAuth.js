f00a8b395c1af4dd2461220b888cd36266de17ad
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-12-24 12:02:04 -0600

    abstracted auth

---
+++ app/scripts/FirebaseAuth.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyBJH9lISpo3B-Grw99476d1jbo7prbKGfU",
+  authDomain: "bloc-chat-e33ba.firebaseapp.com",
+  databaseURL: "https://bloc-chat-e33ba.firebaseio.com",
+  storageBucket: "bloc-chat-e33ba.appspot.com",
+  messagingSenderId: "41547300094"
+};
+firebase.initializeApp(config);
